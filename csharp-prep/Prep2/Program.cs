516eab860416c1ea3433f80945919cb9704e95cb
camden2024 <hen21040@byui.edu>
2025-04-25 12:09:02 -0600

    Created Grade Reader using conditionals

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,47 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+
+        Console.WriteLine("What grade did you receive? ");
+        string grade = Console.ReadLine();
+        int numberGrade = int.Parse(grade);
+
+        string letterGrade = "";
+
+        if (numberGrade >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (numberGrade >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (numberGrade >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (numberGrade >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+
+        if (letterGrade != "F")
+        {
+            Console.WriteLine($"Great you passed the class with {letterGrade}");
+
+        }
+        else
+        {
+            Console.WriteLine("You failed this course, but you can try again for a better grade");
+        }
     }
 }
