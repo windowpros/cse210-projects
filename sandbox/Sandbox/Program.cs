4eaec150a5fbbb08431b03ecac3af2271d38dd7e
camden2024 <hen21040@byui.edu>
2025-04-29 10:51:23 -0600

    commented old code and added writeline stmt

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,15 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        // Console.WriteLine("Hello Sandbox World!");
 
-        Console.WriteLine("Input your first name: ");
-        string firstname = Console.ReadLine();
-        Console.WriteLine($"Your first name is: {firstname}");
+        // Console.WriteLine("Input your first name: ");
+        // string firstname = Console.ReadLine();
+        // Console.WriteLine($"Your first name is: {firstname}");
 
-        Console.WriteLine("Input your last name: ");
-        string lastname = Console.ReadLine();
-        Console.WriteLine($"Your full name is: {lastname}, {firstname} {lastname}");
+        // Console.WriteLine("Input your last name: ");
+        // string lastname = Console.ReadLine();
+        // Console.WriteLine($"Your full name is: {lastname}, {firstname} {lastname}");
+
+        int x = 10;
+        if (x == 10)
+
+        {
+            Console.WriteLine("X is 10");
+        }
+        Console.WriteLine("How are you?");
 
     }
 }
