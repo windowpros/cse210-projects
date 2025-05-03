1cb19deea2fce1c214e65e19c78574d8b195864d
camden2024 <hen21040@byui.edu>
2025-05-03 16:31:59 -0600

    Built while loops Prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,36 @@
 using System;
+using System.Collections;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("What is the magic number? ");
+        string magicNumber = Console.ReadLine();
+        int secretNumber = int.Parse(magicNumber);
+
+    
+        int aGuess = 0;
+
+
+        while (aGuess != secretNumber)
+        {
+            Console.WriteLine("Guess the secret number?");
+            String guess = Console.ReadLine();
+            aGuess = int.Parse(guess);
+            
+            if (aGuess < secretNumber)
+            {
+                Console.WriteLine("You need to guess a higher number.");
+            }
+            else
+            {
+                Console.WriteLine("You need to guess a lower number. ");
+            }
+        }
+        
+         Console.WriteLine("Congrats, you guessed the secret number!");
+
     }
 }
