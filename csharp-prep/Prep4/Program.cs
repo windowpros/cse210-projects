527412ab04c9f0c23f4c2556c0a9414915c445c5
camden2024 <hen21040@byui.edu>
2025-05-03 17:41:34 -0600

    Finished prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Globalization;
 
 class Program
 {
@@ -6,6 +7,51 @@ class Program
     {
         Console.WriteLine("Hello Prep4 World!");
 
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Add numbers to the list. When finished, type 0. ");
+        string contribution = Console.ReadLine();
+        int aContribution = int.Parse(contribution);
+
+        int maximum = 0;
+
+        while (true)
+        {
+            Console.WriteLine("Add another number. ");
+            contribution = Console.ReadLine();
+            aContribution = int.Parse(contribution);
+
+            if (aContribution != 0)
+            {
+                numbers.Add(aContribution);
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        foreach (int number in numbers)
+        {
+            if (number > maximum)
+            {
+                maximum = number;
+            }
+        }
+
+        int sum = 0;
+
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+       
+        int average = sum/numbers.Count;
         
+
+        Console.WriteLine($"Sum = {sum}");
+        Console.WriteLine($"Average = {average}");
+        Console.WriteLine($"Maximum = {maximum}");
     }
 }
