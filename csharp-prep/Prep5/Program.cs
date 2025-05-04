e791a3b7ae82456833aa0b2d02b725d8fd8495ce
camden2024 <hen21040@byui.edu>
2025-05-03 19:05:13 -0600

    Finished ALL Functions Prep5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -5,5 +5,42 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep5 World!");
+
+
+        int finalNumber = GetFavNumber();
+        string finalName = GetUserName();
+        int finalSqr = aFinalSquare(finalNumber);
+        // int finalSqr = finalNumber * finalNumber;
+
+        FinalText(finalName,finalSqr);
+
+        // Console.WriteLine($"Hi {finalName}, your favorite number squared is {finalSqr}");
+
+        static string GetUserName()
+        {
+            Console.WriteLine("What is your UserName? ");
+            string UserName = Console.ReadLine();
+            return UserName;
+        }
+
+        static int GetFavNumber()
+        {
+            Console.WriteLine("What is your favorite Number? ");
+            string FavNumber = Console.ReadLine();
+            int aFavNumber = int.Parse(FavNumber);
+            return aFavNumber;
+        }
+        static int aFinalSquare(int finalNumber)
+        {
+            int testFinalSquare = finalNumber * finalNumber;
+            return testFinalSquare;
+        }
+
+       static void FinalText(string finalName, int finalSqr)
+       {
+        Console.WriteLine($"Welcome, {finalName}, your favorite number Squared is {finalSqr}");
+       }
+
     }
+
 }
