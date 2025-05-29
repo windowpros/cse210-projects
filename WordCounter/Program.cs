284246288917b813103aa28ff4863e4dc7330816
camden2024 <hen21040@byui.edu>
2025-05-29 10:46:35 -0600

    word counter code

---
+++ WordCounter/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hallo Welt!");
+
+        WordCounter wordCounter = new WordCounter("This is a Test Sentence Test Test");
+        wordCounter.DisplayWords();
+
+        int count = wordCounter.CountSingleWord("Test");
+        Console.WriteLine(count);
+
+
+    }
+    
+}
---
+++ WordCounter/WordCounter.cs
@@ -0,0 +1,43 @@
+using System.IO.Compression;
+
+class WordCounter
+{
+    private List<String> _words;
+
+    public WordCounter(string text)
+    {
+        _words = new List<string>();
+        SplitTextIntoWords(text);
+    }
+
+    private void SplitTextIntoWords(string text)
+    {
+        string[] words = text.Split(" ");
+
+        foreach (string word in words)
+        {
+            _words.Add(word);
+        }
+    }
+
+    public void DisplayWords()
+    {
+        foreach (string word in _words)
+        {
+
+        }
+    }
+
+    public int CountSingleWord(string searchword)
+    {
+        int count = 0;
+
+        foreach (string word in _words)
+        {
+            if (word == searchword)
+                count++;
+        }
+
+        return count;
+    }
+}
